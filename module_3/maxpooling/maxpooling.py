174ed96924b6a6b7281639a4eb5b5c74c8f1531f
Andrey Shishkov <shishkoffandrey@yandex.ru>
2022-07-19 01:14:46 +0300

    add small test to maxpooling

--- module_3/maxpooling/maxpooling.py
+++ module_3/maxpooling/maxpooling.py
@@ -28,3 +28,8 @@ class MaxPooling:
                 s = (x for r in matrix[i * sh: i * sh + h] for x in r[j * sw: j * sw + w])
                 res[i][j] = max(s)
         return res
+
+
+mp = MaxPooling(step=(2, 2), size=(2,2))
+res = mp([[1, 2, 3, 4], [5, 6, 7, 8], [9, 8, 7, 6], [5, 4, 3, 2]])    # [[6, 8], [9, 7]]
+assert res == [[6, 8], [9, 7]]
